7d8976311f080df5d81f651fdd3e2e481d2d95db
kambe.kenta <kenta.k199405@gmail.com>
2020-12-12 11:25:23 +0900

    write code 2020/12/12.

--- golang/main.go
+++ golang/main.go
@@ -5,17 +5,95 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"os/user"
 	"regexp"
 	"strings"
 	"sync"
+	"text/template"
 	"time"
 
 	"github.com/go-ini/ini"
 	"golang.org/x/sync/semaphore"
 )
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+// var templates = template.Must(template.ParseFiles("./template/edit.html", "./template/view.html"))
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles("./template/" + tmpl + ".html")
+	t.Execute(w, p)
+
+	// TODO: キャッシュ有効化templateFile
+	// templateFile := "./template/" + tmpl + ".html"
+	// err := templates.ExecuteTemplate(w, templateFile, p)
+	// if err != nil {
+	// 	println(err.Error())
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// }
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-z0-9]+)$")
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
+}
 
 func init() {
 	fmt.Println("Init!")
@@ -339,6 +417,11 @@ func init() {
 }
 func main() {
 	LoggingSettings("test.log")
+
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+	log.Fatal(http.ListenAndServe(":8080", nil))
 
 	b := []byte(`{"name":"mike", "age":20, "nicknames": ["a","b","c"]}`)
 	var p1 PersonMarshal
